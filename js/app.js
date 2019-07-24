d9ba9caa40249d3b9ba284523e2b20b6bc4719bb
Chandan Purbia <chandanpurbia94@gmail.com>
2019-07-25 01:16:09 +0900

    feature: add nav-bar and content dynamically on the page

--- js/app.js
+++ js/app.js
@@ -17,6 +17,23 @@
  * Define Global Variables
  * 
 */
+const page_content = [
+    {
+        header: "Section 1",
+        para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.",
+        para2: "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non."
+    },
+    {
+        header: "Section 2",
+        para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.",
+        para2: "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non."
+    },
+    {
+        header: "Section 3",
+        para1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.",
+        para2: "Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non."
+    },
+]
 
 
 /**
@@ -24,7 +41,25 @@
  * Start Helper Functions
  * 
 */
+function getIdFromString(str){
+    return str.split(" ").join("").toLowerCase()
+}
 
+function createSection(header, para1, para2){
+    return (
+        "<section id="+ getIdFromString(header) +" data-nav=" + header +" class='your-active-class'>" +
+        "<div class='landing__container'>" +
+        "<h2>" + header + "</h2>" +
+        "<p>" + para1 + "</p>" +
+        "<p>" + para2 + "</p>" +
+        "</div>" +
+        "</section>"
+    )
+}
+
+function createNavBarListItem(header){
+    return "<li><a class='menu__link' href=#" + getIdFromString(header) +">" + header + "</a></li>"
+}
 
 
 /**
@@ -34,7 +69,13 @@
 */
 
 // build the nav
+const navbar_list = document.getElementById("navbar__list")
+const main_container = document.querySelector("main#container")
 
+page_content.forEach(function(content) {
+    navbar_list.innerHTML += createNavBarListItem(content.header)
+    main_container.innerHTML += createSection(content.header, content.para1, content.para2)
+})
 
 // Add class 'active' to section when near top of viewport
 
@@ -53,5 +94,7 @@
 // Scroll to section on link click
 
 // Set sections as active
+// Cache selectors
 
-
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
